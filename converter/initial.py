906bc5b74924172728619e62da6b990bdfe29185
Eyasmin Basher <eyasminbasher@gmail.com>
2023-05-09 23:58:46 +0100

    initial commit

---
+++ converter/initial.py
@@ -0,0 +1,12 @@
+# Tutorials are from here https://www.youtube.com/watch?v=kQ8DGP9p2LY
+
+import PySimpleGUI as sg
+
+# sg.Window(title, layout)
+layout = [
+    [1],
+    [2],
+    [3]
+]
+
+sg.Window('Converter', layout)
